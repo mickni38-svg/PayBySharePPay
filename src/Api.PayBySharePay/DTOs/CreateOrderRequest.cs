431950a811223ba3957bd1295c8604f9adc6c4f7
mickni38-svg <mickni38@gmail.com>
2026-09-02 15:57:34 +0200

    Validate UC-05 create-order request

--- src/Api.PayBySharePay/DTOs/CreateOrderRequest.cs
+++ src/Api.PayBySharePay/DTOs/CreateOrderRequest.cs
@@ -7,10 +7,25 @@ public class CreateOrderRequest
     [Required]
     [Range(1, int.MaxValue)]
     public int CreatedByParticipantId { get; set; }
+
     [Required]
+    [StringLength(80, MinimumLength = 1)]
     public string Title { get; set; } = string.Empty;
+
     public string? Category { get; set; }
+
+    [StringLength(500)]
     public string? Message { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
     public int? MerchantParticipantId { get; set; }
+
+    [Required]
+    [MinLength(1)]
     public List<int> ParticipantIds { get; set; } = new();
+
+    [Required]
+    [StringLength(100, MinimumLength = 8)]
+    public string IdempotencyKey { get; set; } = string.Empty;
 }
