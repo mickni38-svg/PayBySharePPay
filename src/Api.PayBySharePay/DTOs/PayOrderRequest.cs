b82e10ee55b480ef0db8503b6b5f86d3e277f943
mickni38-svg <mickni38@gmail.com>
2026-09-02 22:27:20 +0200

    Document JWT authorization for legacy pay

--- src/Api.PayBySharePay/DTOs/PayOrderRequest.cs
+++ src/Api.PayBySharePay/DTOs/PayOrderRequest.cs
@@ -4,6 +4,7 @@ namespace Api.PayBySharePay.DTOs;
 
 public class PayOrderRequest
 {
+    /// <summary>Legacy-kompatibilitet. Ignoreres ved autorisation; brugeridentiteten hentes fra JWT.</summary>
     [Required]
     [Range(1, int.MaxValue)]
     public int RequestingParticipantId { get; set; }
