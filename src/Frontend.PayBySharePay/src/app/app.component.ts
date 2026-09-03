f8951d1b69883ed39eb70161ce48f34d455adf5d
mickni38-svg <mickni38@gmail.com>
2026-09-03 22:44:39 +0200

    ui: remove global home header area

--- src/Frontend.PayBySharePay/src/app/app.component.ts
+++ src/Frontend.PayBySharePay/src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { BottomNavComponent } from './layout/bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, BottomNavComponent],
+  imports: [RouterOutlet, BottomNavComponent],
   template: `
     <div class="app-shell">
-      <header class="app-header">
-        <a routerLink="/home" class="app-header__logo-link">
-          <img class="app-header__logo" src="images/logo.png" alt="PayBySharePay" />
-        </a>
-      </header>
       <main class="app-shell__content">
         <router-outlet></router-outlet>
       </main>
@@ -30,29 +25,6 @@ import { BottomNavComponent } from './layout/bottom-nav/bottom-nav.component';
       position: relative;
     }
 
-    .app-header {
-      display: flex;
-      align-items: center;
-      justify-content: flex-start;
-      padding: 6px 20px 2px;
-      background: #070B14;
-    }
-
-    .app-header__logo-link {
-      display: block;
-      width: 100%;
-      max-width: 320px;
-      text-decoration: none;
-    }
-
-    .app-header__logo {
-      width: 100%;
-      max-width: 320px;
-      height: auto;
-      object-fit: contain;
-      display: block;
-    }
-
     .app-shell__content {
       flex: 1;
       overflow-y: auto;
@@ -63,4 +35,3 @@ import { BottomNavComponent } from './layout/bottom-nav/bottom-nav.component';
 export class AppComponent {
   title = 'PayBySharePay';
 }
-
