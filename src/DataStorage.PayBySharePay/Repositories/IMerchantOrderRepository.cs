c52b0d2ae58190cdee2b75bc449417f9ea9c4a6a
mickni38-svg <mickni38@gmail.com>
2026-09-05 22:02:25 +0200

    feat: add Order Hub merchant queries

--- src/DataStorage.PayBySharePay/Repositories/IMerchantOrderRepository.cs
+++ src/DataStorage.PayBySharePay/Repositories/IMerchantOrderRepository.cs
@@ -5,6 +5,8 @@ namespace DataStorage.PayBySharePay.Repositories;
 public interface IMerchantOrderRepository
 {
     Task<MerchantOrder?> GetBySourceOrderIdAsync(int sourceOrderId, CancellationToken cancellationToken = default);
+    Task<MerchantOrder?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<MerchantOrder>> GetByMerchantAsync(int merchantParticipantId, bool completed, CancellationToken cancellationToken = default);
     Task<MerchantOrder> AddAsync(MerchantOrder merchantOrder, CancellationToken cancellationToken = default);
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
 }
