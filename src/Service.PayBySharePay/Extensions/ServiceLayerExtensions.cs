8a68eb58ee7d6348584e2f5fae0375f70d045abb
mickni38-svg <mickni38@gmail.com>
2026-09-05 21:38:11 +0200

    feat: register square inspired merchant adapter

--- src/Service.PayBySharePay/Extensions/ServiceLayerExtensions.cs
+++ src/Service.PayBySharePay/Extensions/ServiceLayerExtensions.cs
@@ -15,6 +15,7 @@ public static class ServiceLayerExtensions
         services.AddScoped<IMessageService, MessageService>();
         services.AddScoped<IMerchantOrderService, MerchantOrderService>();
         services.AddScoped<IMerchantOrderFinalizationService, MerchantOrderFinalizationService>();
+        services.AddScoped<ISquareInspiredMerchantOrderAdapter, SquareInspiredMerchantOrderAdapter>();
         services.AddScoped<IExternalPaymentService, ExternalPaymentService>();
         services.AddScoped<IParticipantPaymentStateService, ParticipantPaymentStateService>();
         services.AddScoped<IGroupPaymentOrchestrationService, GroupPaymentOrchestrationService>();
